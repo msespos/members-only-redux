07b3e6aa012f0f72c963248a41701ecd665ef3d3
Mike Esposito <mikeesposito6852@gmail.com>
2021-07-26 07:55:46 -0700

    Add before_action, #new, #require_login and #logged_in? to Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
   def new
+    @post = Post.new
   end
 
   def create
@@ -7,4 +9,17 @@ class PostsController < ApplicationController
 
   def index
   end
+
+  private
+
+    def require_login
+      unless logged_in?
+        flash[:error] = "You must be logged in to access this section"
+        redirect_to new_user_session_path # halts request cycle
+      end
+    end
+
+    def logged_in?
+      !current_user.nil?
+    end
 end
